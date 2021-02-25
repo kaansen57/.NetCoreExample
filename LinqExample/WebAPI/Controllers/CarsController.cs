271a1f0866040531f841644b76558e2e7a355486
Kaan SEN <kaansen57@outlook.com>
2021-02-25 23:39:33 +0300

    WebAPI layer added

---
+++ LinqExample/WebAPI/Controllers/CarsController.cs
@@ -0,0 +1,33 @@
+﻿using Business.Abstract;
+using Business.Concrete;
+using Core.Utilities.Results;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/mapi/[controller]")]
+    [ApiController]
+    public class CarsController : ControllerBase
+    {
+        //Loosely coupled , esnek bağlantı  , yani sadece imzaya (interface) ye bağlısın
+        ICarManager _carManager;
+        public CarsController(ICarManager carManager)
+        {
+            _carManager = carManager;
+        }
+        [HttpGet]
+        public List<Car> Get()
+        {
+            //Dependecy chain bağlımlılık zinciri car manager efcardal'a bağımlı
+            //CarManager carManager = new CarManager(new EfCarDal());
+            return _carManager.GetAll(1234).Data;
+        }
+    }
+}
